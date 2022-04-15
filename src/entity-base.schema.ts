17a0200f74e11a5af4e525c2048c1b1f4bbfc000
Frank Weijers <frank@weijers.io>
2022-04-15 16:48:12 +0200

    Fix ts errors

--- src/entity-base.schema.ts
+++ src/entity-base.schema.ts
@@ -1,18 +1,18 @@
 import { CreateDateColumn, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class EntityBaseSchema {
-  constructor(props?: unknown) {
+  protected constructor(props?: unknown) {
     if (props) {
       Object.assign(this, props);
     }
   }
 
   @PrimaryColumn({ update: false })
-  readonly id: string;
+  readonly id!: string;
 
   @CreateDateColumn({ type: 'timestamp', update: false })
-  readonly created_at: Date;
+  readonly created_at!: Date;
 
   @UpdateDateColumn({ type: 'timestamp' })
-  readonly updated_at: Date;
+  readonly updated_at!: Date;
 }
