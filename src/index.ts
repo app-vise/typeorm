766dbbe1725932ededb494a1bffebff2a435ea29
Frank Weijers <frank@weijers.io>
2022-09-24 14:18:43 +0200

    Add BareEntity with SchemaFactory for entities without createdAt + updatedAt fields

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export * from './entity-schema.factory';
-export { EntityBaseSchema } from './entity-base.schema';
-export { Cursor, Paginator } from './paginator';
-export { QueryHelper } from './query-helper';
-export { TypeormRepositoryProvider } from './repository.provider';
-export { TypeormReadRepository } from './typeorm.read.repository';
-export { TypeormRepository } from './typeorm.repository';
-export { TypeormWriteRepository } from './typeorm.write.repository';
+export * from './entity-base.schema';
+export * from './bare-entity-schema.factory';
+export * from './paginator';
+export * from './query-helper';
+export * from './repository.provider';
+export * from './typeorm.read.repository';
+export * from './typeorm.repository';
+export * from './typeorm.write.repository';
 export * from './utils';
