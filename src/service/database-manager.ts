74fcb98eb7bf3c6e3ade4283dc0f9b6c951d91c8
Jay Doerga <jaydoerga@MacBook-Pro-van-Jay.local>
2023-04-21 11:55:43 +0200

    Extra db creation

--- src/service/database-manager.ts
+++ src/service/database-manager.ts
@@ -1,6 +1,18 @@
 import { UUID } from '@appvise/domain';
 
+export enum ExtraDBTypes {
+  IAM = 'iam',
+}
+
 export abstract class DatabaseManager {
-  abstract createDatabase(tenantId: UUID): Promise<string>;
-  abstract deleteDatabase(tenantId: UUID): Promise<boolean>;
+  abstract createMainDatabase(tenantId: UUID): Promise<string>;
+  abstract deleteMainDatabase(tenantId: UUID): Promise<boolean>;
+  abstract createExtraDatabase(
+    tenantId: UUID,
+    type: ExtraDBTypes
+  ): Promise<string>;
+  abstract deleteExtraDatabase(
+    tenantId: UUID,
+    type: ExtraDBTypes
+  ): Promise<boolean>;
 }
